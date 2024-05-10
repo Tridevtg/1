ec3443697256d4eb56895f46325bec1fcf52ca69
haridas008 <156284133+haridas008@users.noreply.github.com>
2024-05-10 07:23:32 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24763783"
 api_hash = "0534b1088b5197e703895f37fb78d311"
-bot_token = "6590318295:AAF6dtNj16phFjaYlzOIOk6i2Wg5y2yrOM0"
+bot_token = "7048845820:AAGM5RlG3nBHZxfuWx1lc58cZlFGzkO0zgc"
