e2abd953765d5b9de826177a9bf01a1d44d78f68
krishnarjun00 <156284133+krishnarjun00@users.noreply.github.com>
2024-05-23 18:26:40 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24763783"
 api_hash = "0534b1088b5197e703895f37fb78d311"
-bot_token = "7110570058:AAFy00r0RZjviC-vtJDbzWywkXH7S0RDB2E"
+bot_token = "7063274332:AAGpOMIUpfqCSy9Bhq-SFIp3elZ-Mj_5bc8"
